3f4a8de0597b46df399cb7d81ed4f24639884202
maz1125 <matsuda.daiki.1125.biz@gmail.com>
2018-03-22 22:38:37 +0900

    refs #5 add eslint

--- webpack.config.js
+++ webpack.config.js
@@ -1,27 +1,29 @@
-var publidDir = __dirname + '/01_udemy_react/public';
+const path = require('path');
+
+const publidDir = path.join(__dirname, '/01_udemy_react/public');
 module.exports = {
   entry: [
-    './01_udemy_react/src/index.js'
+    './01_udemy_react/src/index.js',
   ],
   output: {
     path: publidDir,
     publicPath: '/',
-    filename: 'bundle.js'
+    filename: 'bundle.js',
   },
   module: {
     rules: [{
       exclude: /node_modules/,
       loader: 'babel-loader',
       query: {
-        presets: ['react', 'es2015']
-      }
-    }]
+        presets: ['react', 'es2015'],
+      },
+    }],
   },
   resolve: {
-    extensions: ['.js', '.jsx']
+    extensions: ['.js', '.jsx'],
   },
   devServer: {
     historyApiFallback: true,
-    contentBase: publidDir
-  }
+    contentBase: publidDir,
+  },
 };
