606bf8be445046ec4fe4d044a8385a94c150ff4a
maz1125 <matsuda.daiki.1125.biz@gmail.com>
2018-03-24 22:37:17 +0900

    refs #9 add state and domEvent

--- 01_udemy_react/src/components/classApp.jsx
+++ 01_udemy_react/src/components/classApp.jsx
@@ -1,8 +1,30 @@
 import React, { Component } from 'react';
+import Greeting from './greeting';
 
 class ClassApp extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      name: 'Bob',
+    };
+  }
+
+  handleMouseOver() {
+    this.setState({ name: 'John' });
+  }
+
+  handleMouseOut() {
+    this.setState({ name: 'Hoge' });
+  }
   render() {
-    return (<div>Hello React from clases component</div>);
+    return (
+      <div
+        onMouseOver={() => this.handleMouseOver()}
+        onMouseOut={() => this.handleMouseOut()}
+      >
+        <Greeting name={this.state.name} />;
+      </div>
+    );
   }
 }
 
