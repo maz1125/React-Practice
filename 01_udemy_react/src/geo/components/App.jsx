273a012ee7e627cda04b4f039ec8d17ec3c6e183
maz1125 <matsuda.daiki.1125.biz@gmail.com>
2018-03-28 18:14:08 +0900

    refs #14 add geo-result area, import new scss

--- 01_udemy_react/src/geo/components/App.jsx
+++ 01_udemy_react/src/geo/components/App.jsx
@@ -1,12 +1,13 @@
 import React, { Component } from 'react';
 import SearchForm from './SearchForm';
+import GeoResult from './GeoResult';
 
 class App extends Component {
   constructor(props) {
     super(props);
     this.state = {};
   }
-  hundleOnSubmit(place){
+  hundleOnSubmit(place) {
     console.log(place);
   }
   render() {
@@ -14,6 +15,11 @@ class App extends Component {
       <div>
         <h2 className="header">緯度経度検索</h2>
         <SearchForm onSubmit={place => this.hundleOnSubmit(place)} />
+        <GeoResult
+          address={this.state.address}
+          lat={this.state.lat}
+          lng={this.state.lng}
+        />
       </div>
     );
   }
---
+++ 01_udemy_react/src/geo/components/GeoResult.jsx
@@ -0,0 +1,25 @@
+import React, { PropTypes } from 'react';
+
+function GeoResult({ address, lat, lng} ) {
+  return (
+    <ui className="geo-result">
+      <li>住所：{address}</li>
+      <li>緯度：{lat}</li>
+      <li>経度：{lng}</li>
+    </ui>
+  );
+}
+
+GeoResult.propTypes = {
+  address: PropTypes.string,
+  lat: PropTypes.number,
+  lng: PropTypes.number,
+};
+
+GeoResult.defaultProps = {
+  address: '',
+  lat: '',
+  lng: '',
+};
+
+export default GeoResult;
