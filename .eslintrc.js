84627dc7d923119d73da64d5229b8e65691c1b30
maz1125 <matsuda.daiki.1125.biz@gmail.com>
2018-04-19 16:28:31 +0900

    refs #32 add eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,5 +2,9 @@ module.exports = {
     "extends": "airbnb",
     "env":{
       "browser":true,
+    },
+    "rules":{
+      "no-underscore-dangle": [
+        "error", { "allow": ["__REDUX_DEVTOOLS_EXTENSION__"] }]
     }
 };
